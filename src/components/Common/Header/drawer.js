44fa2c366a5a28e9225ca5947486aad12dfdf0b7
Hamza110376 <Muhammadhamzaawan110@gmail.com>
2024-04-03 18:00:51 +0500

    navbar done

---
+++ src/components/Common/Header/drawer.js
@@ -0,0 +1,39 @@
+import * as React from "react";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
+import { useState } from "react";
+
+
+
+export default function AnchorTemporaryDrawer() {
+const [open, setOpen] = useState(false)
+
+ return (
+   <>
+     <div>
+       <Button onClick={() => setOpen(true)}>
+         <MenuRoundedIcon className="link" />
+       </Button>
+       <Drawer anchor={"right"} open={open} onClose={() => setOpen(false)}>
+         <div className="drawer-div">
+           <a href="/">
+             <p className="link">Home</p>
+           </a>
+           <a href="/">
+             <p className="link">Compare</p>
+           </a>
+           <a href="/">
+             <p className="link">Watchlist</p>
+           </a>
+           <a href="/">
+             <p className="link">Dashboard</p>
+           </a>
+         </div>
+       </Drawer>
+     </div>
+   </>
+ );
+  };
+  
+ 
